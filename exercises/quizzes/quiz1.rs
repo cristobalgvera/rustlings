5fae85711ae490bf0d84e9902121b0447bd806a1
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-09-04 08:20:08 -0400

    feat(quiz): complete first quiz

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,8 +12,23 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+use std::cmp::Ordering;
+
 fn main() {
     // You can optionally experiment here.
+}
+
+fn calculate_price_of_apples(apples_quantity: u16) -> u16 {
+    const MINIMUM_APPLES_QUANTITY_TO_DISCOUNT: u16 = 41;
+    const REGULAR_PRICE_OF_APPLES: u16 = 2;
+    const PRICE_OF_APPLES_WITH_DISCOUNT: u16 = 1;
+
+    let price_of_apples = match apples_quantity.cmp(&MINIMUM_APPLES_QUANTITY_TO_DISCOUNT) {
+        Ordering::Less => REGULAR_PRICE_OF_APPLES,
+        _ => PRICE_OF_APPLES_WITH_DISCOUNT,
+    };
+
+    apples_quantity * price_of_apples
 }
 
 // Don't change the tests!
