7468b6c1381bb84b9c056a9ce7ec09e6b6f39424
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2024-10-12 16:54:10 -0300

    feat(quiz): complete second quiz

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,21 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut transformed_values = Vec::new();
+
+        for (value, command) in input {
+            let transformed_value = match command {
+                Command::Uppercase => value.to_uppercase(),
+                Command::Trim => value.trim().to_string(),
+                Command::Append(times) => value + &"bar".repeat(times),
+            };
+
+            transformed_values.push(transformed_value);
+        }
+
+        transformed_values
+    }
 }
 
 fn main() {
@@ -37,8 +51,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
